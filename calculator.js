46e416a8e0df1103c57b03b04fb162fedd520065
astashenkov <ya.astashenkov@gmail.com>
2021-12-04 19:47:36 +0300

    Add logic for equals, plus and minus buttons

--- calculator.js
+++ calculator.js
@@ -1,5 +1,3 @@
-// 
-
 'use strict'
 
 const input = document.querySelector('input');
@@ -8,9 +6,17 @@ const changeSign = document.querySelector('.CS');
 const backSpase = document.querySelector('.BS');
 const allClean = document.querySelector('.AC');
 const coma = document.querySelector('.coma');
+const plus = document.querySelector('.plus');
+const minus = document.querySelector('.minus');
+const divide = document.querySelector('.divide');
+const multiply = document.querySelector('.multiply');
+const equals = document.querySelector('.equals');
 
 const statusCalc = {
-    coma: 'false',
+    coma: false,
+    value: '',
+    math: '',
+    new: false
 }
 
 
@@ -18,6 +24,10 @@ input.value = 0;
 
 numbers.forEach((item) => {
     item.addEventListener('click', () => {
+        if (statusCalc.new === true) {
+            input.value = '';
+            statusCalc.new = false;
+        }
         if (input.value.length < 12) {
             if (input.value === '0') {
                 input.value = item.textContent;
@@ -38,19 +48,74 @@ changeSign.addEventListener('click', () => {
 
 backSpase.addEventListener('click', () => {
     if (input.value.length === 1) {
+        if (input.value[0] === ',') {
+            statusCalc.coma = false;
+        }
         input.value = 0;
     } else {
+        if (input.value.substring(input.value.length - 1, input.value.length) === ',') {
+            statusCalc.coma = false;
+        }
         input.value = input.value.substring(0, input.value.length - 1);
     }
 });
 
 allClean.addEventListener('click', () => {
+    if (input.value.includes(',')) {
+        statusCalc.coma = false;
+    }
     input.value = 0;
+    statusCalc.value = '';
+    statusCalc.math = '';
+    statusCalc.new = false;
+    input.value = '';
 });
 
-coma.document.addEventListener('click', () => {
-    if (statusCalc.coma === 'false') {
+coma.addEventListener('click', () => {
+    if (statusCalc.coma === false) {
         input.value = input.value + ',';
         statusCalc.coma = true;
+    }
+});
+
+equals.addEventListener('click', () => {
+    switch (statusCalc.math) {
+        case '+': 
+            input.value = +(statusCalc.value) + +(input.value);
+            break;
+        case '-': 
+            input.value = Number(statusCalc.value) - Number(input.value);
+            break;
+        case '*': 
+            input.value = Number(statusCalc.value) * Number(input.value);
+            break;
+        case '/': 
+            input.value = Number(statusCalc.value) / Number(input.value);
+            break;
+    }
+    statusCalc.new = true;
+});
+
+plus.addEventListener('click', () => {
+    statusCalc.new = true;
+    statusCalc.coma = false;
+    if (statusCalc.value) {
+        input.value = Number(statusCalc.value) + Number(input.value);
+        statusCalc.value = input.value;
+    } else {
+        statusCalc.value = input.value;
+        statusCalc.math = '+';
+    }
+});
+
+minus.addEventListener('click', () => {
+    statusCalc.new = true;
+    statusCalc.coma = false;
+    if (statusCalc.value) {
+        input.value = Number(statusCalc.value) - Number(input.value);
+        statusCalc.value = input.value;
+    } else {
+        statusCalc.value = input.value;
+        statusCalc.math = '-';
     }
 });
